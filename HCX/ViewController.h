e88fd091a555885c5c75b126812fc26484c2871a
cocoaHong <hongn_ok@163.com>
2016-03-21 21:54:25 +0800

    Initial Commit

---
+++ HCX/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCX
+//
+//  Created by 洪晨曦 on 16/3/21.
+//  Copyright © 2016年 CocoaHCX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HCX/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HCX
+//
+//  Created by 洪晨曦 on 16/3/21.
+//  Copyright © 2016年 CocoaHCX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
